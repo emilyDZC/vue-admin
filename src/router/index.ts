e9d88ec9fac482cd658ff1428ca611b632872838
Emily Chaplais <emilychaplais@hotmail.com>
2022-05-18 21:52:13 +0100

    Add users table and pagination

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,13 +4,21 @@ import HomeView from "../views/HomeView.vue";
 import Layout from "@/views/Layout.vue";
 import Login from "@/views/Login.vue";
 import Register from "@/views/Register.vue";
+import Users from "@/views/Users.vue";
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   { path: "/login", component: Login },
   { path: "/register", component: Register },
-  { path: "", component: Layout },
+  {
+    path: "",
+    component: Layout,
+    children: [
+      { path: "", redirect: "/users" },
+      { path: "/users", component: Users },
+    ],
+  },
 ];
 
 const router = new VueRouter({
