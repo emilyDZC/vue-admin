863058d319204a16ab1b7eb060ad5f501afa3932
Emily Chaplais <emilychaplais@hotmail.com>
2022-05-18 20:58:39 +0100

    Everything up to logout

---
+++ src/views/Layout.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-app>
+    <Nav :user="user" />
+    <div class="container-fluid">
+      <div class="row">
+        <Menu />
+        <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+          <div class="table-responsive">
+            <table class="table table-striped table-sm">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Header</th>
+                  <th scope="col">Header</th>
+                  <th scope="col">Header</th>
+                  <th scope="col">Header</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1,001</td>
+                  <td>random</td>
+                  <td>data</td>
+                  <td>placeholder</td>
+                  <td>text</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </main>
+      </div>
+    </div>
+  </v-app>
+</template>
+
+<script>
+import Nav from "@/components/Nav.vue";
+import Menu from "@/components/Menu.vue";
+import axios from "axios";
+import { User } from "@/models/user";
+
+export default {
+  components: { Nav, Menu },
+  data() {
+    return {
+      user: new User(),
+    };
+  },
+  async mounted() {
+    try {
+      const { data } = await axios.get("user");
+      this.user = data;
+    } catch (error) {
+      await this.$router.push("/login");
+    }
+  },
+};
+</script>
+
+<style></style>
